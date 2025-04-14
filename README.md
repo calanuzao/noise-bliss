d85b8fda5b093005d79e64a34fd4add5116c3b33
calanuzao <calanuzao@gmail.com>
2025-04-14 06:16:40 -0400

    upd: plotting dataset

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # noise-bliss
 Denoising Algorithms for Acoustic Scene Classification in Deep Learning Models
-usin the SONYC Dataset by MARL @NYU
 
-### This dataset requires a Multi-Class Classification Model.
+### Audio is Multi-Class so changin los function is crucial
 
 TODO:
-* Train part of the model by next week
+* Schedule a meeting with Prof. Fuentes
+    * models.py
+    * Train a portion of a dataset $/rightarrow$ move forward
+
 * Different branches for organization
 
 * Classification 
@@ -20,5 +22,3 @@ TODO:
     * Further divide test, validation, training out of temporal data scale
     * Call model for classification
         * Embedding
-
-* Schedule a Meeting with Prof. Fuentes
--- utils.py
+++ utils.py
@@ -5,8 +5,12 @@ import keras
 import numpy as np
 import random
 import librosa
+import librosa.display
 import cv2
 import os
+import datetime
+import pandas as pd
+from pathlib import Path
 
 ### ============= Data Generation Functions ============= ###
 
@@ -22,7 +26,7 @@ def load_data(data_path):
     Tuple[np.ndarray, np.ndarray]
         A tuple of the two numpy arrays.
         The first array contains the file paths of the audio files, 
-        and the second array contains their corresponding lables.
+        and the second array contains their corresponding labels.
     """
 
     if isinstance(data_path, bytes):
@@ -87,42 +91,191 @@ def process_audio(file_path, sr=22050, duration=None):
 
     return spectrogram_dB
 
+# temporal data
+def construct_datetime(df):
+    """
+    Constructs a 'datetime' column from 'year'. 'week', 'day', 'hour'
+    Retunrs the modified DataFrame.
+    """
+    dt_series = df.apply(lambda row: datetime.datetime.strptime(
+        f"{int(row['year'])} {int(row['week']) + 1} {int(row['day']) + 1}",
+        "%G %V %u"                                      
+    ) + pd.Timedelta(hours=int(row['hour'])), axis=1) 
+
+    df['datetime'] = pd.to_datetime(dt_series)
+    return df
+
+# location id
+def construct_location_id(df):
+    """
+    A unique 'location_id' column from 'sensor_id', 'borough', and 'block'.
+    Returns the modified DataFrame.
+    """
+    df['location_id'] = (
+        df['borough'].astype(str) + "_" +
+        df['block'].astype(str) + "_" +
+        df['sensor_id'.astype(str)]
+    )
+    return df
+
 # creating dataset
-def data_set(data_dir, batch_size, target_shape, shuffle=True):
+def data_set(file_list, batch_size, target_shape, shuffle=True):
     """
-    Creates dataset from the audio files
+    Creates dataset from the list of audio files
     """
-
-    audio_files = []
-    labels = []
-    class_names = sorted(os.listdir(data_dir))
-
-    for class_idx, class_name in enumerate(class_names):
-        class_dir = os.path.join(data_dir, class_name)
-        for audio_file in os.listdir(class_dir):
-            if audio_file.endswith(('.wav')):
-                audio_files.append(os.path.join(class_dir, audio_file))
-                labels.append(class_idx)
-
     def generator():
-        for audio_file, label in zip(audio_files, labels):
-            spe = process_audio(audio_file)
+        for audio_file in file_list:
+            spec = process_audio(audio_file)
             if spec is not None:
                 spec = tf.image.resize(spec[..., np.newaxis], target_shape)
-                yield spec, label
+                yield spec
 
-    dataset = tf.data.Dataset.from_generator(
-        generator,
-        output_signature=(
-            tf.TensorSpec(shape=(target_shape[0], target_shape[1], 1), dtype=tf.float32),
-            tf.TensorSpec(shape=(), dtype=tf.int32)
+        dataset = tf.data.Dataset.from_generator(
+            generator,
+            output_signature=tf.TensorSpec(
+                shape=(target_shape[0], target_shape[1], 1),
+                dtype=tf.float32       
+            )
         )
+
+        if shuffle:
+            dataset = dataset.shuffle(buffer_size=1000)
+
+        dataset = dataset.batch(batch_size)
+        dataset = dataset.prefetch(tf.data.AUTOTUNE)
+
+        return dataset
+
+### ============= Plotting ============= ###
+
+def plot_distribution(data, xlabel, ylabel, title, kind='bar', xticks=None):
+    plt.figure(figsize=(10,4))
+    if kind == 'bar':
+        plt.bar(data.index, data.values, color='steelblue')
+    elif kind == 'line':
+        plt.plot(data.index, data.values, marker='o')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    if xticks is not None:
+        plt.xticks(xticks)
+    plt.grid(True, axis='y')
+    plt.tight_layout()
+    plt.show()
+
+def compute_and_plot_distributions(df):
+    """
+    Plots hourly, daily, weekly, monthly, and yearly distributions.
+    """
+    hourly = df['datetime'].dt.hour.value_counts().reindex(range(24), fill_value=0).sort_index()
+    daily = df['datetime'].dt.date.value_counts().sort_index()
+    weekly = df['datetime'].dt.isocalendar().week.value_counts().sort_index()
+    monthly = df['datetime'].dt.month.value_counts().reindex(range(1, 13), fill_value=0).sort_index()
+    yearly = df['datetime'].dt.year.value_counts().sort_index()
+
+    plot_distribution(hourly, "Hour of Day", "Recordings", "Hourly Distribution", xticks=range(24))
+    plot_distribution(daily, "Date", "Recordings", "Daily Distribution", kind='line')
+    plot_distribution(weekly, "ISO Week", "Recordings", "Weekly Distribution")
+    plot_distribution(monthly, "Month", "Recordings", "Monthly Distribution", xticks=range(1, 13))
+    plot_distribution(yearly, "Year", "Recordings", "Yearly Distribution")
+
+def compute_and_plot_location_distributions(df):
+    """
+    Plots sensor-based and borough-based distributions.
+    Assumes 'location_id' and 'borough' columns exists
+    """
+    # count by sensor
+    sensor_counts = df['sensor_id'].value_counts().sort_index()
+
+    # count by borough
+    borough_counts = df['borough'].value_counts().sort_index()
+
+    # count by full location_id
+    location_counts = df['location_id'].value_counts().sort_index()
+
+    plot_distribution(sensor_counts, "Sensor ID", "Recordings", "Recording per Sensor")
+    plot_distribution(borough_counts, "Borough Code", "Recordings", "Recording per Borough")
+    plot_distribution(location_counts, "Locaiton ID", "Recordings", "Recording per Unique Location", kind='bar')
+
+def plot_distributions_by_split(df):
+    """
+    Plots hourly, daily, weekly, monthly, and yearly distribution by split.
+    """
+    split_colors = {
+        'train': '#E69F00',     
+        'validate': '#56B4E9',  
+        'test': '#009E73'       
+    }
+
+    time_granularities = {
+        'Hourly': df['datetime'].dt.hour,
+        'Daily': df['datetime'].dt.date,
+        'Weekly': df['datetime'].dt.isocalendar().week,
+        'Monthly': df['datetime'].dt.month,
+        'Yearly': df['datetime'].dt.year
+    }
+
+    for name, series in time_granularities.items():
+        plt.figure(figsize=(12, 4))
+
+        for split in df['split'].unique():
+            subset = df[df['split'] == split]
+            counts = series[subset.index].value_counts().sort_index()
+
+            plt.plot(
+                counts.index,
+                counts.values,
+                label=split.capitalize(),
+                color=split_colors.get(split, 'black'),
+                linewidth=1
+            )
+
+        plt.title(f"{name} Distribution by Split")
+        plt.xlabel(name)
+        plt.ylabel("Number of Recordings")
+        plt.legend(title="Split")
+        plt.grid(True, axis='y', linestyle='--', alpha=0.6)
+        plt.tight_layout()
+        plt.show()
+
+def mel_spectrogram(audio, sample_rate=2250, n_mels=128, hop_length=512):
+    """
+    Computes the normalized Mel spectrogram of an audio signal.
+
+    Parameters
+    ----------
+    audio : np.ndarray
+        Input audio signal as a 1D numpy array.
+    sample_rate : int, optional
+        Sampling rate of the audio signal, by default 22050.
+    n_mels : int, optional
+        Number of Mel bands to generate, by default 128.
+    hop_length : int, optional
+        Number of samples between successive frames, by default 512.
+
+    Returns
+    -------
+    np.ndarray
+        Mel spectrogram as a 2D numpy array.
+    """
+    audio = librosa.to_mono(audio)
+    mel_spectrogram = librosa.feature.melspectrogram(
+        y=audio,                
+        sr=sample_rate,         
+        hop_length=hop_length,  
+        n_mels=n_mels           
     )
+
+    log_spectrogram = librosa.power_to_db(mel_spectrogram)
+    librosa.display.specshow(
+        log_spectrogram,
+        x_axis='time',
+        y_axis='mel', 
+        sr=sample_rate, 
+        fmax=8000
+        )
     
-    if shuffle:
-        dataset = dataset.shuffle(buffer_size=1000)
-
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(tf.data.AUTOTUNE)
-
-    return dataset
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel spectrogram')
+    plt.tight_layout()
+    plt.show()
